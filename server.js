2f95f7d206036aea1c317828f228bde369e77b00
Simcha Sucot <simchasucot@gmail.com>
2023-12-24 17:23:55 +0200

    update

--- server.js
+++ server.js
@@ -2,41 +2,50 @@ import mongoose from "mongoose";
 import express from "express";
 import path from "path";
 import http from "http";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { Server } from "socket.io";
 import cors from "cors";
-import  {createNewMessage} from './db/createMessage.js'
-import getMessagesBetweenUsers from "./db/findMessage.js"
+import { createNewMessage } from "./db/createMessage.js";
+import getMessagesBetweenUsers from "./db/findMessage.js";
 import { log } from "console";
-
+import axios from "axios"
 
 dotenv.config();
 const app = express();
 const PORT = 3000;
 app.use(express.json());
 
+app.get("/getUsers", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://infra-jerusalem-1-server.vercel.app/users/allUsers/"
+    );
+    console.log(response.data);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-
-
-app.post('/sendMasseg', async (req, res) => {
+app.post("/sendMasseg", async (req, res) => {
   const messageObjectFromClient = req.body;
   const chek = await createNewMessage(messageObjectFromClient);
-  console.log(chek)
+  console.log(chek);
   res.status(200).send(chek);
-})
+});
 
-app.get('/findMessages', async (req, res) => {
+app.get("/findMessages", async (req, res) => {
   const me = req.query.me;
   const selected = req.query.selected;
-  const messagesUser = await getMessagesBetweenUsers(me, selected)
-  res.status(200).send(messagesUser)
-})
+  const messagesUser = await getMessagesBetweenUsers(me, selected);
+  res.status(200).send(messagesUser);
+});
 
 const server = http.createServer(app);
 app.use(cors());
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:4174","http://localhost:4181"],
     methods: ["GET", "POST"],
   },
 });
@@ -44,42 +53,38 @@ const io = new Server(server, {
 const usersMap = new Map();
 
 io.on("connection", (socket) => {
- console.log(socket.id,'conected')
-
+  console.log(socket.id, "conected");
 
   socket.on("userId", (userId) => {
     usersMap.set(userId, socket.id);
-    console.log(usersMap)
+    console.log(usersMap);
   });
 
   socket.on("privetMessage", (message) => {
-    createNewMessage({message})
+    createNewMessage({ message });
 
-  try{
-    let recipiantSctId = usersMap.get(message.to)
+    try {
+      let recipiantSctId = usersMap.get(message.to);
 
-    socket.to(recipiantSctId).emit("message", message);
-  }
-  catch (error) {
-      console.error('the recipient is not conected now', error);
+      socket.to(recipiantSctId).emit("message", message);
+    } catch (error) {
+      console.error("the recipient is not conected now", error);
     }
-  
   });
 
-  function getUserIdBySctId(sctId){
+  function getUserIdBySctId(sctId) {
     usersMap.forEach((value, key) => {
       if (value === sctId) {
-        return key
-   
+        return key;
       }
     });
   }
 
   socket.on("disconnect", () => {
-    const user = getUserIdBySctId(socket)
+    const user = getUserIdBySctId(socket);
     usersMap.delete(user);
 
-    console.log(socket.id,"disconnected");
+    console.log(socket.id, "disconnected");
   });
 });
 
